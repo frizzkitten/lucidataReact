94ba3f314455bff5432d6aae52eee755f3f9f123
Kevin Quinn <kpquinn2@wisc.edu>
2018-03-22 14:12:01 -0500

    start of parseSMS

--- App.js
+++ App.js
@@ -93,6 +93,30 @@ export default class App extends Component<Props> {
         })
     }
 
+    // Messages come in as "w,words in message", where "w" denotes the api
+    parseSMS(sms) {
+        let splitSms = sms.split(",");
+        let api = splitSms[0];
+        body = splitSms[1];
+
+       switch(api) {
+           case "w":
+               console.log("Chose Wikipedia case");
+               return "w:" + body;
+               break;
+           case "s":
+               console.log("Chose Sports case");
+               return "s:" + body;
+               break;
+           case "e":
+               console.log("Chose Weather case");
+               return "e:" + body;
+               break;
+           default:
+               console.log("Hit default case, no api chosen");
+       }
+       return "";
+    }
 
     render() {
         let infoCounter = 0;
@@ -141,3 +165,4 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
